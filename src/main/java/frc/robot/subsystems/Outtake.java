89c12952a5a2dfd38a2a62a2c6659c62eff62b66
Philippe Boulanger <philboul28@gmail.com>
2024-01-25 11:19:57 -0500

    feat(Outtake): Attempts at implementing a PID to control the Outtake's position

--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -9,15 +9,27 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class Outtake extends SubsystemBase {
 
-  private final CANSparkMax m_leftOuttakeMotor = new CANSparkMax(Constants.Outtake.leftOuttakeMotorPort, MotorType.kBrushless);
-  private final CANSparkMax m_rightOuttakeMotor = new CANSparkMax(Constants.Outtake.rightOuttakeMotorPort, MotorType.kBrushless);
+  private final CANSparkMax m_leftOuttakeMotor = new CANSparkMax(Constants.Outtake.leftOuttakeMotorCANPort, MotorType.kBrushless);
+  private final CANSparkMax m_rightOuttakeMotor = new CANSparkMax(Constants.Outtake.rightOuttakeMotorCANPort, MotorType.kBrushless);
 
-  private final CANSparkMax m_feederMotor = new CANSparkMax(Constants.Outtake.feederMotorPort, MotorType.kBrushless);
+  private final VictorSPX m_feederMotor = new VictorSPX(Constants.Outtake.feederMotorCANPort);
+
+
+  private final CANSparkMax m_wristMotor = new CANSparkMax(Constants.Outtake.wristMotorCANPort, MotorType.kBrushless);
+
+  private final SparkPIDController m_pidController = m_wristMotor.getPIDController();
+  private final RelativeEncoder m_relativeEncoder = m_wristMotor.getEncoder();
+  private double m_targetRotation = 0;
 
   private double m_shootingSpeed = Constants.Outtake.shootingSpeed;
   private double m_feedingSpeed = Constants.Outtake.feedingSpeed;
@@ -25,17 +37,54 @@ public class Outtake extends SubsystemBase {
   private double m_currentFeedingSpeed = m_feedingSpeed;
 
   /** Creates a new Outtake. */
-  public Outtake() {}
+  public Outtake() {
+    m_wristMotor.restoreFactoryDefaults();
+    m_wristMotor.setSmartCurrentLimit(20);
+
+    m_relativeEncoder.setPosition(0.0);
+    m_pidController.setP(Constants.Outtake.kP);
+    m_pidController.setI(Constants.Outtake.kI);
+    m_pidController.setD(Constants.Outtake.kD);
+
+    m_wristMotor.burnFlash();
+
+    m_wristMotor.set(0);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     SmartDashboard.putNumber("Feeder speed", m_currentFeedingSpeed);
+    SmartDashboard.putNumber("WristPosition", m_wristMotor.getEncoder().getPosition());
   }
 
-  public void setFeederSpeed(double feedSpeed) {
-    m_currentFeedingSpeed = feedSpeed;
-    m_feederMotor.set(feedSpeed);
+  public void goToPosition(double targetPosition) {
+    m_pidController.setReference(targetPosition, ControlType.kPosition);
+  }
+
+  public void stopWristAtCurrentPosition() {
+    m_pidController.setReference(m_relativeEncoder.getPosition(), ControlType.kPosition);
+  }
+
+  public void raiseWrist() {
+    m_wristMotor.set(Constants.Outtake.wristRaisingSpeed);
+  }
+
+  public void lowerWrist() {
+    m_wristMotor.set(Constants.Outtake.wristLoweringSpeed);
+  }
+
+  public void setWristSpeed(double speed) {
+    m_wristMotor.set(speed);
+  }
+
+  public void stopWrist() {
+    m_wristMotor.set(0);
+  }
+
+  public void startShooterAmp() {
+    m_leftOuttakeMotor.set(-0.2 * m_shootingSpeed);
+    m_rightOuttakeMotor.set(0.2 * m_shootingSpeed);
   }
 
   public void startShooter() {
@@ -50,11 +99,16 @@ public class Outtake extends SubsystemBase {
 
   public void startFeeder() {
     m_currentFeedingSpeed = m_feedingSpeed;
-    m_feederMotor.set(m_feedingSpeed);
+    m_feederMotor.set(VictorSPXControlMode.PercentOutput, m_feedingSpeed);
+  }
+
+  public void setFeederSpeed(double feedSpeed) {
+    m_currentFeedingSpeed = feedSpeed;
+    m_feederMotor.set(VictorSPXControlMode.PercentOutput, feedSpeed);
   }
 
   public void stopFeeder(){
     m_currentFeedingSpeed = 0;
-    m_feederMotor.set(0);
+    m_feederMotor.set(VictorSPXControlMode.PercentOutput, 0);
   }
 }
